22c7df3f01b45bc1a27355d6f0d0de6468d11439
inebenitez <101297551+inebenitez@users.noreply.github.com>
2022-10-05 23:27:46 -0300

    backend inicio

---
+++ ap/backend/src/test/java/com/portfolio/fib/FibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.fib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
